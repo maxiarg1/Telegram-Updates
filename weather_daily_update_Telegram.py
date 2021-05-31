ae92eb6a4f1d93de10f8ad00c656e4fd848f50ac
maxiarg1 <79663430+maxiarg1@users.noreply.github.com>
2021-05-31 14:30:38 -0300

    Update weather_daily_update_Telegram.py

--- weather_daily_update_Telegram.py
+++ weather_daily_update_Telegram.py
@@ -5,8 +5,8 @@ import schedule
 
 def bot_send_text(bot_message):
     
-    bot_token = '1784682772:AAGQLQKZzRcall0nbORSo7Iv7jncKZZoFe0'
-    bot_chatID = '-544661504'
+    bot_token = 'TOKEN'
+    bot_chatID = 'chatID'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
